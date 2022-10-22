b1381d886c473faa29e4c2cee88737217d605aaf
Daniel Clements <drclements@liberty.edu>
2022-10-22 19:00:19 -0400

    Added Titles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "All we have to decide is what to do with the time that is given us";
 
 document.querySelector("body").appendChild(h2);
