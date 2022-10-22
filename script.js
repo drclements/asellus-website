a0133620b70d4bbbd1b9aba102b6db38b623090c
Daniel Clements <drclements@liberty.edu>
2022-10-22 19:58:45 -0400

    add photo & descript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "All we have to decide is what to do with the time that is given us";
+h2.textContent = '"All we have to decide is what to do with the time that is given us"';
 
 document.querySelector("body").appendChild(h2);
