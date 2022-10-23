ccb8856ae79f139fe1eeb822ec98fd02a1f54366
Daniel Clements <drclements@liberty.edu>
2022-10-22 22:36:26 -0400

    add photo/input button

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const h2 = document.createElement("h2");
 h2.textContent = '"All we have to decide is what to do with the time that is given us"';
 
 document.querySelector("header").appendChild(h2);
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('Thanks for visiting!');
+}
+
+input.addEventListener('click', clickAlert);
